42b97327b9c87eed8b6c54eec3fe2c15f35f03fe
allenstr90 <allenstr90@gmail.com>
2021-07-25 11:58:57 -0400

    fix: complete

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+### Completed !!!
+
 ### Assignment
 
 In this project you will use object oriented programming to create a Rectangle class and a Square class. The Square class should be a subclass of Rectangle and inherit methods and attributes.
--- shape_calculator.py
+++ shape_calculator.py
@@ -1,6 +1,51 @@
 class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    def set_width(self, width):
+        self.width = width
+
+    def set_height(self, height):
+        self.height = height
+
+    def get_area(self):
+        return self.width * self.height
+
+    def get_perimeter(self):
+        return 2 * self.width + 2 * self.height
+
+    def get_diagonal(self):
+        return (self.width ** 2 + self.height ** 2) ** .5
+
+    def get_picture(self):
+        if self.width > 50 or self.height > 50:
+            return 'Too big for picture.'
+        res = ''.ljust(self.width, '*')
+        for y in range(1, self.height):
+            res += f'\n{"".ljust(self.width, "*")}'
+        return res + '\n'
+
+    def __str__(self):
+        return f'Rectangle(width={self.width}, height={self.height})'
+
+    def get_amount_inside(self, shape):
+        return int(self.get_area() / shape.get_area())
 
 
+class Square(Rectangle):
+    def __init__(self, length):
+        super().__init__(length, length)
+        self.side = length
 
+    def set_side(self, side):
+        self.side = self.height = self.width = side
 
-class Square:
+    def set_width(self, width):
+        self.side = width
+
+    def set_height(self, height):
+        self.side = height
+
+    def __str__(self):
+        return f'Square(side={self.side})'
